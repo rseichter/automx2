3b5c699ddeb343b6655576883137bc85efb1558d
Ralph Seichter <s@sys4.de>
2019-12-05 18:17:51 +0100

    Setup script now accepts venv path

--- contrib/setupvenv.sh
+++ contrib/setupvenv.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 # vim:tabstop=4:noexpandtab
 #
-# Creates a Python 3.7 virtual environment in the current directory.
+# Creates a Python 3 virtual environment. The target directory can be passed
+# as a parameter. The default path is 'venv' in the current directory.
+
+dir="${1:-venv}"
 
 set -e
-if [ -d venv ]; then
-	echo "Directory 'venv' already exists, exiting." >&2
+if [ -d "${dir}" ]; then
+	echo "Directory '${dir}' already exists, exiting." >&2
 	exit 1
 fi
-python3.7 -m venv venv
-source venv/bin/activate
-pip install -U pip
-pip install -U setuptools
+python3 -m venv "${dir}"
+source "${dir}/bin/activate"
+
+set +e
+pip install -U pip setuptools || true
