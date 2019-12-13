493486e7cc7a226352692e7b700c0d8c916b145b
Ralph Seichter <s@sys4.de>
2019-12-13 18:47:17 +0100

    Script can now generate HTML docs

--- contrib/package.sh
+++ contrib/package.sh
@@ -2,20 +2,32 @@
 # vim:tabstop=4:noexpandtab
 #
 # Script to package automx2 for distribution and to handle PyPI uploads.
-# You need Python modules 'wheel' and 'twine' to use this script.
+# You need Python modules 'wheel' and 'twine' to publish to PyPI, and
+# Ruby Gems 'asciidoctor' and 'asciidoctor-diagram' to generate HTML
+# documentation.
 
 set -e
 
 function usage() {
 	local bn
 	bn="$(basename $0)"
-	echo "Usage: ${bn} {clean | dist}" >&2
+	echo "Usage: ${bn} {clean | dist | doc}" >&2
 	echo "       ${bn} upload [repository]" >&2
 	exit 1
 }
 
+function do_clean() {
+	/bin/rm -r build/* dist/*
+}
+
 function do_dist() {
 	python setup.py sdist bdist_wheel
+}
+
+function do_doc() {
+	pushd doc >/dev/null
+	asciidoctor -r asciidoctor-diagram automx2.adoc
+	popd >/dev/null
 }
 
 function do_upload() {
@@ -36,8 +48,8 @@ function do_upload() {
 arg="$1"
 shift
 case "$arg" in
-	clean)
-		rm -r build/* dist/*
+	clean | doc)
+		do_$arg
 		;;
 	dist | upload)
 		source venv/bin/activate
