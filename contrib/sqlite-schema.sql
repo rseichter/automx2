97d1b2385075b0e5339710e047a9ca658ba38a69
Ralph Seichter <github@seichter.de>
2021-10-04 17:19:30 +0200

    Update SQLite schema

--- contrib/sqlite-schema.sql
+++ contrib/sqlite-schema.sql
@@ -1,49 +1,49 @@
 CREATE TABLE provider (
 	id INTEGER NOT NULL, 
-	name VARCHAR NOT NULL, 
-	short_name VARCHAR NOT NULL, 
+	name VARCHAR(128) NOT NULL, 
+	short_name VARCHAR(32) NOT NULL, 
 	PRIMARY KEY (id)
 );
 CREATE TABLE server (
 	id INTEGER NOT NULL, 
 	prio INTEGER DEFAULT '10' NOT NULL, 
-	name VARCHAR NOT NULL, 
+	name VARCHAR(128) NOT NULL, 
 	port INTEGER NOT NULL, 
-	type VARCHAR NOT NULL, 
-	socket_type VARCHAR NOT NULL, 
-	user_name VARCHAR NOT NULL, 
-	authentication VARCHAR NOT NULL, 
+	type VARCHAR(32) NOT NULL, 
+	socket_type VARCHAR(32) NOT NULL, 
+	user_name VARCHAR(64) NOT NULL, 
+	authentication VARCHAR(32) NOT NULL, 
 	PRIMARY KEY (id)
 );
 CREATE TABLE davserver (
 	id INTEGER NOT NULL, 
-	url VARCHAR NOT NULL, 
+	url VARCHAR(128) NOT NULL, 
 	port INTEGER NOT NULL, 
-	type VARCHAR NOT NULL, 
+	type VARCHAR(32) NOT NULL, 
 	use_ssl BOOLEAN NOT NULL, 
 	domain_required BOOLEAN NOT NULL, 
-	user_name VARCHAR, 
+	user_name VARCHAR(64), 
 	PRIMARY KEY (id), 
 	CHECK (use_ssl IN (0, 1)), 
 	CHECK (domain_required IN (0, 1))
 );
 CREATE TABLE ldapserver (
 	id INTEGER NOT NULL, 
-	name VARCHAR NOT NULL, 
+	name VARCHAR(128) NOT NULL, 
 	port INTEGER NOT NULL, 
 	use_ssl BOOLEAN NOT NULL, 
-	search_base VARCHAR NOT NULL, 
-	search_filter VARCHAR NOT NULL, 
-	attr_uid VARCHAR NOT NULL, 
-	attr_cn VARCHAR, 
-	bind_password VARCHAR, 
-	bind_user VARCHAR, 
+	search_base VARCHAR(128) NOT NULL, 
+	search_filter VARCHAR(128) NOT NULL, 
+	attr_uid VARCHAR(32) NOT NULL, 
+	attr_cn VARCHAR(32), 
+	bind_password VARCHAR(128), 
+	bind_user VARCHAR(128), 
 	PRIMARY KEY (id), 
 	CHECK (use_ssl IN (0, 1))
 );
 CREATE TABLE domain (
 	id INTEGER NOT NULL, 
-	name VARCHAR NOT NULL, 
+	name VARCHAR(128) NOT NULL, 
 	provider_id INTEGER NOT NULL, 
 	ldapserver_id INTEGER, 
 	PRIMARY KEY (id), 
