0d65099ef5f88e4ab40de82f96c2b7d65a6080c0
Ralph Seichter <github@seichter.de>
2026-02-17 08:14:06 +0100

    Remove obsolete import

--- src/automx2/database.py
+++ src/automx2/database.py
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with automx2. If not, see <https://www.gnu.org/licenses/>.
 """
 
-import sys
 from typing import Optional
 
 from automx2 import PLACEHOLDER_ADDRESS
